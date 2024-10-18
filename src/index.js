89b7ba61970da2ba10375a16a751e3ccb4058475
V-Kozintsev <v_kozintcev@mail.ru>
2024-10-18 15:48:06 +0300

    configuring packages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import "./index.css";
+import './index.css';
 
-console.log("Webpack работает!");
+console.log('Webpack работает!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,48 @@
-const path = require("path");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Очищает output папку перед новым сборкой
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.js$/, // Применяется ко всем .js файлам
+        exclude: /node_modules/, // Исключить папку node_modules
+        use: {
+          loader: 'babel-loader', // Используем babel-loader для обработки JS
+        },
+      },
+      {
+        test: /\.css$/, // Регулярное выражение для нахождения файлов .css
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/, // Для обработки изображений и шрифтов
+        type: 'asset/resource', // Webpack 5 использует asset module
       },
     ],
   },
-  mode: "development", // или 'production' для продакшн-сборки
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true, // Включает сжатие
+    port: 9000, // Порт для dev server
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.css'], // Автоматическое добавление расширений
+  },
 };
